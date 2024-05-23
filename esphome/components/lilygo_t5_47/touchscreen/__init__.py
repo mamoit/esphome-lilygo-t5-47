9f7b2fbeb9626a0cec7f98f80e99318fdff690e6
Miguel Almeida <migaxmoitax@gmail.com>
2024-05-23 18:30:26 +0100

    Register touchscreen component

--- esphome/components/lilygo_t5_47/touchscreen/__init__.py
+++ esphome/components/lilygo_t5_47/touchscreen/__init__.py
@@ -4,6 +4,7 @@ import esphome.config_validation as cv
 from esphome import pins
 from esphome.components import i2c, touchscreen
 from esphome.const import CONF_ID
+from esphome.const import __version__ as ESPHOME_VERSION
 
 from .. import lilygo_t5_47_ns
 
@@ -37,7 +38,8 @@ CONFIG_SCHEMA = touchscreen.TOUCHSCREEN_SCHEMA.extend(
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
+    if cv.Version.parse(ESPHOME_VERSION) < cv.Version.parse("2023.12.0"):
+        await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
     await touchscreen.register_touchscreen(var, config)
 
